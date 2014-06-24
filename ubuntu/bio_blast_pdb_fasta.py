6b181bb408fe5d46a6f37f06fad36b0d36247463
sbvermae <stijn.vermaere@gmail.com>
2014-06-24 21:13:44 +0200

    test push on windows

--- ubuntu/bio_blast_pdb_fasta.py
+++ ubuntu/bio_blast_pdb_fasta.py
@@ -20,6 +20,7 @@ choose_pdbChoices = []
 
 class MyFrame1 ( wx.Frame ):
 
+
 	def __init__( self, parent ):
 		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"Scop3D - BLAST", pos = wx.DefaultPosition, size = wx.Size( 567,328 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
